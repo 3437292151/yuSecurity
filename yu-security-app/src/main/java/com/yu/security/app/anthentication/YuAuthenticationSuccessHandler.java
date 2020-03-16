57bad3a5c0967ce598e5bb75d30039d9390e8c80
yuchanglong <3437292151@qq.com>
2020-03-16 13:19:33 +0800

    app登录相关代码

--- yu-security-app/src/main/java/com/yu/security/app/anthentication/YuAuthenticationSuccessHandler.java
+++ yu-security-app/src/main/java/com/yu/security/app/anthentication/YuAuthenticationSuccessHandler.java
@@ -3,10 +3,18 @@ package com.yu.security.app.anthentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yu.security.core.properties.LoginType;
 import com.yu.security.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +37,12 @@ public class YuAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     private SecurityProperties securityProperties;
 
     @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices tokenServices;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
     @Override
@@ -36,13 +50,63 @@ public class YuAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
                                         HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("登录成功！！");
 
-        if (LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-            return;
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
+        }
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+
+        if (clientDetails == null){
+            throw new UnapprovedClientAuthenticationException("client对应的配置信息不存在：" + clientId);
+        }else if(!StringUtils.equals(clientSecret, clientDetails.getClientSecret())){
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配：" + clientId);
         }
 
-        super.onAuthenticationSuccess(request, response, authentication);
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken accessToken = tokenServices.createAccessToken(oAuth2Authentication);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(accessToken));
+
+    }
+
+    /**
+     * Decodes the header into a username and password.
+     *
+     * @throws BadCredentialsException if the Basic header is not present or is not valid
+     * Base64
+     */
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        }
+        catch (IllegalArgumentException e) {
+            throw new BadCredentialsException(
+                    "Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, "UTF-8");
+
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[] { token.substring(0, delim), token.substring(delim + 1) };
     }
 
 }
